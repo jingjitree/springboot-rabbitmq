6040c3cdba1adfd4bb076462532e6a03d1f57e4b
jingjitree <jingjitree@gmail.com>
2021-05-14 15:37:57 +0800

    '数据源配置'

--- src/main/java/top/inson/rabbitmq/core/DruidConfiguration.java
+++ src/main/java/top/inson/rabbitmq/core/DruidConfiguration.java
@@ -1,14 +1,31 @@
 package top.inson.rabbitmq.core;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DruidConfiguration {
+
+    @Bean(initMethod = "init", destroyMethod = "close")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
+    public DataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
 
     @Bean
     public ServletRegistrationBean<StatViewServlet> druidStatViewServlet(){
@@ -31,6 +48,27 @@ public class DruidConfiguration {
         //添加不需要忽略的格式信息
         filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico");
         return filterRegistrationBean;
+    }
+
+    @Bean
+    @Primary
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception{
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    @Primary
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory){
+        SqlSessionTemplate template = new SqlSessionTemplate(sqlSessionFactory);
+        return template;
+    }
+
+    @Bean
+    @Primary
+    public DataSourceTransactionManager transactionManager(DataSource dataSource){
+        return new DataSourceTransactionManager(dataSource);
     }
 
 }
--- src/main/java/top/inson/rabbitmq/core/TkMybatisConfiguration.java
+++ src/main/java/top/inson/rabbitmq/core/TkMybatisConfiguration.java
@@ -1,10 +1,12 @@
 package top.inson.rabbitmq.core;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @Configuration
 @MapperScan("top.inson.rabbitmq.dao")
+@EnableTransactionManagement
 public class TkMybatisConfiguration {
 
 
